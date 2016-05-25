a78176776fb60227aa7bd0e8465de600693bb87d
sean1975 <sean1975@gmail.com>
2016-05-25 22:40:59 +1000

    Refactor code and add unit test for issue #2

--- test_waves.py
+++ test_waves.py
@@ -17,26 +17,34 @@ class MockResponse():
     
     
 class WavesTest(unittest.TestCase):
-    fd = None
-    
-    def setUp(self):
-        self.fd = open("test_response.json")
         
-    def tearDown(self):
-        self.fd.close
+    @mock.patch('waves.urllib2')
+    def testQuery(self, mock_urllib2):
+        fd = open("test_response.json") 
+        mock_urllib2.urlopen.return_value = MockResponse(200, fd)
+        page = MainPage()
+        response = page.query()
+        self.assertTrue(len(response) > 0, "Failed to get waves data")
+        fd.close()
+        
+    def testString2Dict(self):
+        fd_response_string = open("test_response_string.json");
+        response = fd_response_string.read()
+        page = MainPage()
+        result = page.string2dict(response)
+        self.assertTrue(result["records"][0]["_id"] == 3148)
+        self.assertTrue(len(result["records"]) == 100)
+        fd_response_string.close();
         
     @mock.patch('waves.urllib2')
     def testGetWavesData(self, mock_urllib2):
-        mock_urllib2.urlopen.return_value = MockResponse(200, self.fd)
+        fd = open("test_response.json") 
+        mock_urllib2.urlopen.return_value = MockResponse(200, fd)
         page = MainPage()
-        response = page.getWavesData()
-        self.assertTrue(len(response) > 0, "Failed to get waves data")
-        
-    def testString2Dict(self):
-        response = self.fd.read()
-        page = MainPage()
-        response_dict = page.string2dict(response)
-        self.assertTrue(response_dict["result"]["records"][0]["_id"] == 3148)
+        result = page.getWavesData()
+        self.assertTrue(result["records"][0]["_id"] == 3202)
+        #self.assertTrue(len(result["records"]) == 356)        
+        fd.close()
         
 
 if __name__ == "__main__":
--- waves.py
+++ waves.py
@@ -8,11 +8,21 @@ from operator import itemgetter
 class MainPage(webapp2.RequestHandler):
     def getWavesData(self):
         # query all waves statistics for Cairns
-        data = urllib.quote(json.dumps({'resource_id': '2bbef99e-9974-49b9-a316-57402b00609c', 'q': 'cairns'}))
-
+        response = self.query()
+        
+        # load HTTP response into a json dictionary
+        result = self.string2dict(response)
+        
+        return result
+        
+        
+    def query(self):
         # data API: https://data.qld.gov.au/dataset/coastal-data-system-near-real-time-wave-data/resource/2bbef99e-9974-49b9-a316-57402b00609c
         url = 'https://data.qld.gov.au/api/action/datastore_search'
     
+        # query string
+        data = urllib.quote(json.dumps({'resource_id': '2bbef99e-9974-49b9-a316-57402b00609c', 'q': 'cairns'}))
+
         # send HTTP request
         response = urllib2.urlopen(url, data)
         assert response.code == 200
@@ -20,23 +30,18 @@ class MainPage(webapp2.RequestHandler):
         return response.read()
         
     
-    def render(self, data):
+    def render(self, fields, records):
         self.response.headers['Content-Type'] = 'text/plain'
-        result = data['result']
-        # fields = [ _id, Site, SiteNumber, Seconds, DateTime, Latitude, Longitude, Hsig, Hmax, Tp, Tz, SST, Direction, _full_count, rank ]
-        fields = result['fields']
         for field in fields :
             self.response.write(field['id'] + "\t")
         self.response.write("\n")
-        records = result['records'];
         
         for record in records :
             for field in fields :
                 self.response.write(record[field['id']]);
                 self.response.write("\t")
             self.response.write("\n")
-            
-                        
+                                    
 
     def string2dict(self, response):
         response_dict = json.loads(response)
@@ -44,17 +49,18 @@ class MainPage(webapp2.RequestHandler):
         assert response_dict['success'] is True
         records = sorted(response_dict['result']['records'], key=itemgetter("_id"))
         response_dict['result']['records'] = records;
-        return response_dict
+        return response_dict['result']
+
 
     def get(self):
         # get waves data from QLD website
-        response = self.getWavesData()
-        
-        # load HTTP response into a json dictionary
-        response_dict = self.string2dict(response)
+        result = self.getWavesData()
         
         # print the result
-        self.render(response_dict)
+        # fields = [ _id, Site, SiteNumber, Seconds, DateTime, Latitude, Longitude, Hsig, Hmax, Tp, Tz, SST, Direction, _full_count, rank ]
+        fields = result['fields']
+        records = result['records'];
+        self.render(fields, records)
             
         
 app = webapp2.WSGIApplication([
